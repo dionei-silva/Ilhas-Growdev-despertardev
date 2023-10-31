cb82e789078221a888d2f75538bb9d308305229c
Dionei Silva <dionei.silva@gmail.com>
2023-10-31 17:07:33 -0300

    Exercicio ilhas growdev ok

---
+++ script.js
@@ -0,0 +1,50 @@
+/* Os caixas eletrônicos das Ilhas Growdev operam com todos os tipos de
+notas disponíveis, mantendo um estoque de cédulas para cada valor. Os
+clientes do banco utilizam os caixas eletrônicos para efetuar retiradas de
+um certo número inteiro de GrowCoins.
+Sua tarefa é escrever um algoritmo que, dado o valor de GrowCoins
+desejado pelo cliente, determine o número de cada uma das notas
+necessárias para totalizar esse valor. Por exemplo, se o Marcelo deseja
+retirar GC$ 50,00 basta entregar uma única nota de cinquenta GrowCoins.
+Se o Édson deseja retirar GC$ 72,00 será necessário entregar uma nota de
+GC$ 50,00, duas de GC$ 10,00 e duas de GC$ 1,00
+
+Exemplo de entrada:
+72
+Exemplo de saída:
+GC$ 50,00 -> 1
+GC$ 10,00 -> 2
+GC$ 5,00 -> 0
+GC$ 1,00 -> 2 */
+
+
+let nota50 = 0 
+let nota10 = 0 
+let nota5 = 0 
+let nota1 = 0 
+
+let saque = parseInt(prompt("Digite o valor do saque em growcoins"))
+
+while(saque >= 50){
+      saque = saque - 50
+      nota50++
+}
+while(saque >=10){
+      saque = saque -10
+      nota10++
+}
+while(saque >=5){
+      saque = saque -5
+      nota5++
+}
+while(saque >=1){
+      saque = saque - 1
+      nota1++
+}
+
+document.write(`A quantidade de notas 50 sacadas é GC$ ${nota50}<br>`)
+document.write(`A quantidade de notas 10 sacadas é GC$ ${nota10}<br>`)
+document.write(`A quantidade de notas 5 sacadas é GC$ ${nota5}<br>`)
+document.write(`A quantidade de notas 1 sacadas é GC$ ${nota1}<br>`)
+
+
